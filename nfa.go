eb1b4a796b4840523f25a0d25b70d9e30b15c675
Adam Kokhnover <adam.kokhnover@gmail.com>
2025-06-16 17:48:06 +0300

    nfa/toNfa-method
    creates a NFA from given regex

--- nfa.go
+++ nfa.go
@@ -5,3 +5,36 @@ type state struct {
 	terminal    bool
 	transitions map[uint8][]*state
 }
+
+const epsilonChar uint8 = 0 // empty char
+
+func toNfa(ctx *parseContext) *state {
+	startState, endState := tokenToNfa(&ctx.tokens[0])
+
+	for i := 1; i < len(ctx.tokens); i++ {
+		startNext, endNext := tokenToNfa(&ctx.tokens[i])
+		endState.transitions[epsilonChar] = append(
+			endState.transitions[epsilonChar],
+			startNext,
+		)
+		endState = endNext
+	}
+
+	start := &state{
+		transitions: map[uint8][]*state{
+			epsilonChar: {startState},
+		},
+		start: true,
+	}
+	end := &state{
+		transitions: map[uint8][]*state{},
+		terminal:    true,
+	}
+
+	endState.transitions[epsilonChar] = append(
+		endState.transitions[epsilonChar],
+		end,
+	)
+
+	return start
+}
